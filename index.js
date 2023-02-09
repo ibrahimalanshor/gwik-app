66deccdb6b2a6d6f4a193648f5c268430a7fdd64
ibra <ibrahimalanshor6@gmail.com>
2023-02-09 11:41:44 +0800

    feat: resources message

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const { Server } = require('gwik');
 const serverConfig = require('./config/server.config');
 const routes = require('./src/routes');
+const messages = require('./resources/messages');
 
 const server = new Server({
   port: serverConfig.port,
   logging: serverConfig.logging,
+  t9n: {
+    locale: serverConfig.translation.defaultLocale,
+    fallbackLocale: serverConfig.translation.defaultFallbackLocale,
+    messages,
+  },
 });
 
 server.addStaticRoute(serverConfig.static.path, serverConfig.static.dir);
