4d237983e6bc5175fee76adebfed2b7354f2323d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-02-12 03:01:15 -0800

    feat: create server lib

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const { Server } = require('gwik');
-const serverConfig = require('./config/server.config');
-const routes = require('./src/routes');
-const messages = require('./resources/messages');
+const { createServer } = require('./lib/gwik/server');
 
-const server = new Server({
-  port: serverConfig.port,
-  logging: serverConfig.logging,
-  t9n: {
-    locale: serverConfig.translation.defaultLocale,
-    fallbackLocale: serverConfig.translation.defaultFallbackLocale,
-    messages,
-  },
-});
-
-server.addStaticRoute(serverConfig.static.path, serverConfig.static.dir);
-server.addRoute(routes);
+const server = createServer();
 
 server.listen();
